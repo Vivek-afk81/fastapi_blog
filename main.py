2bf2974c91b049f358d6c7f77d3a083661d29e2b
Vivek-afk81 <vivekchauhan560999@gmail.com>
2026-07-01 13:20:50 +0530

    to_dos API initialization

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastApi
+from fastapi import FastAPI
 from pydantic import BaseModel
 
-app=FastApi()
+app=FastAPI()
 
-todos=[]
+to_dos=[]
+
+class Todo(BaseModel):
+    id:int
+    title:str
+    completed:bool
+
+#create API
+@app.post("/to_dos")
+def create_to_dos(todo:Todo):
+    to_dos.append(todo)
+    return {"message" : "TO-DO added","data":todo }
+
+#get API
+app

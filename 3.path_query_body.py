ab06bd61a583d55ffca29de89615a65242f527d0
Vivek-afk81 <vivekchauhan560999@gmail.com>
2026-07-05 12:34:28 +0530

    Add CRUD operations for Todo

---
+++ 3.path_query_body.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+users=[]
+
+class User(BaseModel):
+    name:str
+    age:int
+
+@app.post("/users")
+def create_user(user:User):
+    users.append(user)
+
+    return {
+        "message":"User created",
+        "data":user
+    }
+
+@app.put("/users/{user_id}")
+def update_user(user_id:int,user:User,notify:bool=False): #combination of path parameter query parameter and request body
+    if user_id < len(users):
+        users[user_id]=user
+
+        return {
+            "message":"User Updated",
+            "notify":notify,
+            "data":user
+        }
+    return {
+        "error":"user not found"
+    }
